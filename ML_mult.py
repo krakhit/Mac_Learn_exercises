578faa4d6f4f4f94e6fe0fa039998e782f904367
Karthik Inbasekar <61501745+krakhit@users.noreply.github.com>
2021-11-07 16:21:34 +0200

    Update ML_mult.py

--- ML_mult.py
+++ ML_mult.py
@@ -11,5 +11,11 @@ print('Eigenvalues and Eigenvectors: ',LA.eig(A1))
 print('Determinant ',LA.det(A1))
 print('Inverse: ', A1_inv)
 print('A1.A1^(-1)= I',res)
-print(A1.shape)
-print(A1.size)
+
+#by default numpy doesnt differentiate between row and column vector, it is important that we tell 
+#in most cases it is ok. but if one needs, this is a way to do it.
+r1 = np.array([[1,2,3,4]]) #(1,4)
+r2 = np.array([[5],[6],[7],[8]]) #(4.1)
+print(r1,'\n', r2)
+print(np.matmul(r2,r1)) # gives a matrix
+print(np.matmul(r1,r2)) # gives a number
